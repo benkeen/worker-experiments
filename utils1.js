929121e5d4ccb35bfa5b0f777537fdcd5c6d9fcd
Ben Keen <ben.keen@gmail.com>
2020-07-11 21:18:07 -0700

    test

--- utils1.js
+++ utils1.js
@@ -1,7 +1,60 @@
-var addBreak = function (str) {
-	return str + '<Br />';
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+
+var __assign = function() {
+	__assign = Object.assign || function __assign(t) {
+		for (var s, i = 1, n = arguments.length; i < n; i++) {
+			s = arguments[i];
+			for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+		}
+		return t;
+	};
+	return __assign.apply(this, arguments);
 };
 
-var utils1 = {
-	addBreak: addBreak
+function __spreadArrays() {
+	for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+	for (var r = Array(s), k = 0, i = 0; i < il; i++)
+		for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+			r[k] = a[j];
+	return r;
+}
+
+var getUnique = function (arr) { return arr.filter(function (v, i, a) { return a.indexOf(v) === i; }); };
+// returns an empty array of a particular size
+var getArrayOfSize = function (size) { return __spreadArrays(Array(size)); };
+var removeItem = function (arr, value) {
+	var i = 0;
+	while (i < arr.length) {
+		if (arr[i] === value) {
+			arr.splice(i, 1);
+		}
+		else {
+			++i;
+		}
+	}
+	return arr;
+};
+
+var arrayUtils = /*#__PURE__*/Object.freeze({
+	__proto__: null,
+	getUnique: getUnique,
+	getArrayOfSize: getArrayOfSize,
+	removeItem: removeItem
+});
+
+var utils = {
+	arrayUtils: __assign({}, arrayUtils)
 };
